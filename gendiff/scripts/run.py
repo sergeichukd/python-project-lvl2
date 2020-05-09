3651c54c33e03cd9d89c7d3d509fa181d1924e2b
Sergeichuk Dmitrii <sergeychukdmitry@gmail.com>
2020-05-10 00:40:10 +0300

    flag --format has been added to run script and testpypi url has been corrected

--- gendiff/scripts/run.py
+++ gendiff/scripts/run.py
@@ -3,8 +3,12 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', type=str, nargs=1, help='the first comparable file')
-    parser.add_argument('second_file', type=str, nargs=1, help='the second comparable file')
+    parser.add_argument('first_file', type=str, nargs=1, help='the first comparable file')  # noqa: E501
+    parser.add_argument('second_file', type=str, nargs=1, help='the second comparable file')  # noqa: E501
+    parser.add_argument('-f', '--format', type=str, nargs=1, help='set format of output')  # noqa: E501
     args = parser.parse_args()
     print(args.first_file)
     print(args.second_file)
+
+
+main()
