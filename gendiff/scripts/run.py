ab56dba9e02f3058d72d89f8e8af5f2239baa688
Sergeichuk Dmitrii <sergeychukdmitry@gmail.com>
2020-05-11 00:49:17 +0300

    beta version of generate_diff function

--- gendiff/scripts/run.py
+++ gendiff/scripts/run.py
@@ -1,4 +1,70 @@
 import argparse
+import json
+
+
+def make_common_key_diffs(file1_data, file2_data, *, common_keys):
+    diff_lines = []
+
+    for key in common_keys:
+        if file1_data[key] == file2_data[key]:
+            file_line = f"  {key}: {file2_data[key]}"
+            diff_lines.append(file_line)
+        else:
+            file1_line = f"- {key}: {file1_data[key]}"
+            file2_line = f"+ {key}: {file2_data[key]}"
+            diff_lines.extend([file1_line, file2_line])
+    return tuple(diff_lines)
+
+
+def make_removed_key_diffs(file_data, *, removed_keys):
+    diff_lines = []
+
+    for key in removed_keys:
+        file_line = f"- {key}: {file_data[key]}"
+        diff_lines.append(file_line)
+    return tuple(diff_lines)
+
+
+def make_added_key_diffs(file_data, *, added_keys):
+    diff_lines = []
+
+    for key in added_keys:
+        file_line = f"+ {key}: {file_data[key]}"
+        diff_lines.append(file_line)
+    return tuple(diff_lines)
+
+
+def generate_diff(file1, file2):
+    diff_lines = []
+
+    file1_data = json.load(open(file1))
+    file2_data = json.load(open(file2))
+
+    file1_keys = set(file1_data.keys())
+    file2_keys = set(file2_data.keys())
+
+    common_keys = file1_keys & file2_keys
+    removed_keys = file1_keys - file2_keys
+    added_keys = file2_keys - file1_keys
+
+    for key in common_keys:
+        if file1_data[key] == file2_data[key]:
+            file_line = f"  {key}: {file2_data[key]}"
+            diff_lines.append(file_line)
+        else:
+            file1_line = f"- {key}: {file1_data[key]}"
+            file2_line = f"+ {key}: {file2_data[key]}"
+            diff_lines.extend([file1_line, file2_line])
+
+    for key in removed_keys:
+        file1_line = f"- {key}: {file1_data[key]}"
+        diff_lines.append(file1_line)
+
+    for key in added_keys:
+        file2_line = f"+ {key}: {file2_data[key]}"
+        diff_lines.append(file2_line)
+
+    return "{\n" + "\n".join(diff_lines) + "\n}"
 
 
 def main():
@@ -11,4 +77,5 @@ def main():
     print(args.second_file)
 
 
-main()
+out = generate_diff("before.json", "after.json")
+print(out)
